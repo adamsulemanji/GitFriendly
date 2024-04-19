3839ecfaa3f4a162b85f4e2d8471e06fd14715be
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-04-19 14:03:15 -0500

    Setting up flask server

--- server/main.py
+++ server/main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+
+@app.route("/clean", methods=['POST'])
+def clean():
+    data = request.json
+    url = data.get('url', '')
+    print(url)
+    return jsonify({"cleanedUrl": "Cleaned URL: " + url})
+
+if __name__ == "__main__":
+  app.run(debug=True)
